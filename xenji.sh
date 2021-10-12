569535da9ec69f0e84ab655cb0a3eafc3797c6ff
Kenji <92344291+XennChan@users.noreply.github.com>
2021-10-12 07:53:59 +0700

    Create xenji.sh

---
+++ xenji.sh
@@ -0,0 +1,21 @@
+- 👋 Hi, I’m @XennChan
+- 👀 I’m interested in ...
+- 🌱 I’m currently learning ...
+- 💞️ I’m looking to collaborate on ...
+- 📫 How to reach me ...
+
+<!---
+XennChan/XennChan is a ✨ special ✨ repository because its `README.md` (this file) appears on your GitHub profile.
+You can click the Preview link to take a look at your changes.
+--->
+bot run
+
+pkg update && pkg upgrade
+pkg install bash
+pkg install nodejs
+pkg install ffmpeg
+termux-setup-storage
+cd /sdcard
+cp -r (nama File) $HOME
+cd (nama File)
+node xenji
